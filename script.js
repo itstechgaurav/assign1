4d29b35e2489738c13442f83f5cbe5b4b6f291bb
ankitab48 <ankitab48@gmail.com>
2021-02-22 17:11:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+context.fillStyle = "rgba(255, 0, 0, 1)";
+context.strokeStyle = "black";
+context.lineWidth = "4";
+context.fillRect(50, 50, 100, 100);
+context.strokeRect(50, 50, 100, 100);
+
+
+
+
+
+
+
